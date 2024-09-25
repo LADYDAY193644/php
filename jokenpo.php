08c8e93577d738998efff8926e577511a49b15a5
Programação PHP 3612952.2024.4 <36129522024.4@senacrjedu.local>
2024-09-24 21:52:37 -0300

    aula 7

---
+++ jokenpo.php
@@ -0,0 +1,28 @@
+<?php
+$jogador1 = "pedra";
+$jogador2 = " papel";
+
+if($jogador1 === "pedra"){
+if($jogador2==="pedra"){
+    echo "empate \n";
+}
+else if ($jogador2 === "papel"){
+    echo "jogador 2 ganhou \n ";
+}
+else if ($jogador2 ==="tesoura"){
+    echo "jogador 1 ganhou \n";
+}
+
+}
+if($jogador1 === "p"){
+if($jogador2==="pedra"){
+        echo "empate \n";
+    }
+    else if ($jogador2 === "papel"){
+        echo "jogador 2 ganhou \n ";
+    }
+    else if ($jogador2 ==="tesoura"){
+        echo "jogador 1 ganhou \n";
+    }
+    
+    }
